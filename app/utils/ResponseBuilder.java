2908045df6772a9afdf22fa6f03bacd0b3ac0f81
sohel <sohel.cse.rymec@gmail.com>
2025-02-24 14:20:58 +0530

    changed response structure

--- app/utils/ResponseBuilder.java
+++ app/utils/ResponseBuilder.java
@@ -13,12 +13,12 @@ import java.util.Map;
 public class ResponseBuilder {
 
     public static Map<String, Object> buildResponse(String resmsgId,String statusMsg,int code,String message) {
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("id", "Api.read");
         response.put("ver", "3.0.5");
         response.put("time", LocalDateTime.now());
 
-        HashMap<String, Object> params = new HashMap<>();
+        HashMap<String, Object> params = new LinkedHashMap<>();
         params.put("resmsgId", resmsgId);
         params.put("status", statusMsg);
 
@@ -30,12 +30,12 @@ public class ResponseBuilder {
     }
 
     public static Map<String, Object> buildResponseOnget(String statusMsg,int code,String message,List datasets) {
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("id", "Api.read");
         response.put("ver", "3.0.5");
         response.put("time", LocalDateTime.now());
 
-        HashMap<String, Object> params = new HashMap<>();
+        HashMap<String, Object> params = new LinkedHashMap<>();
 //        params.put("resmsgId", resmsgId);
         params.put("status", statusMsg);
 
@@ -48,12 +48,12 @@ public class ResponseBuilder {
     }
 
     public static Map<String, Object> buildResponseOngetById(String statusMsg,int code,String message,Datasets datasets) {
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("id", "Api.read");
         response.put("ver", "3.0.5");
         response.put("time", LocalDateTime.now());
 
-        HashMap<String, Object> params = new HashMap<>();
+        HashMap<String, Object> params = new LinkedHashMap<>();
 //    params.put("resmsgId", resmsgId);
         params.put("status", statusMsg);
 
